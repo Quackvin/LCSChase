ffbcfcd3c47ea6545a0dfbfdca07b4a868e066c7
Scott McCormack <scott.mccormack@outlook.com>
2017-09-28 21:09:12 +0800

    Minor formatting

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import rules
 screen = pygame.display.set_mode((width, height))
 pygame.display.flip()
 
-player = playerClass.Player(10,10)
-enemy = playerClass.Enemy(40,50)
+player = playerClass.Player(10, 10)
+enemy = playerClass.Enemy(40, 50)
 
-ruleset = rules.Ruleset(100,0.9,0.5)
+ruleset = rules.Ruleset(100, 0.9, 0.5)
 ruleset.initiliseSet()
 
 running = True
@@ -21,30 +21,28 @@ while running:
             if event.key == pygame.K_q:
                 running = False
             if event.key == pygame.K_r:
-                player = playerClass.Player(10,10)
-                enemy = playerClass.Enemy(40,50)
+                player = playerClass.Player(10, 10)
+                enemy = playerClass.Enemy(40, 50)
 
-                ruleset = rules.Ruleset(50,0.9,0.5)
+                ruleset = rules.Ruleset(50, 0.9, 0.5)
                 ruleset.initiliseSet()
             if event.key == pygame.K_LEFT:
-                player.moveH(-10,300)
+                player.moveH(-10, 300)
             if event.key == pygame.K_RIGHT:
-                player.moveH(10,300)
+                player.moveH(10, 300)
             if event.key == pygame.K_UP:
-                    player.moveV(10,300)
+                player.moveV(10, 300)
             if event.key == pygame.K_DOWN:
-                player.moveV(-10,300)
+                player.moveV(-10, 300)
 
-    screen.fill((50,100,200))
+    screen.fill((50, 100, 200))
 
-    ruleset.actionRules(player, enemy, 300,300)
+    ruleset.actionRules(player, enemy, 300, 300)
     ruleset.GA()
     ruleset.remove(0.4)
 
-    pygame.draw.rect(screen, (255,255,255), (player.x,player.y,10,10))
-    pygame.draw.rect(screen, (255,0,0), (enemy.x,enemy.y,10,10))
+    pygame.draw.rect(screen, (255, 255, 255), (player.x, player.y, 10, 10))
+    pygame.draw.rect(screen, (255, 0, 0), (enemy.x, enemy.y, 10, 10))
     pygame.display.flip()
 
-
 pygame.quit()
-
--- playerClass.py
+++ playerClass.py
@@ -1,39 +1,41 @@
 import math
 
+
 class Player(object):
-	def __init__(self, x, y):
-		self.x = float(x)
-		self.y = float(y)
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
 
-	def moveV(self, s, maxY):
-		self.y -= s
-		if self.y > maxY:
-			self.y = maxY - 10
-		elif self.y < 0:
-			self.y = 0
+    def moveV(self, s, maxY):
+        self.y -= s
+        if self.y > maxY:
+            self.y = maxY - 10
+        elif self.y < 0:
+            self.y = 0
 
-	def moveH(self, s, maxX):
-		self.x += s
-		if self.x > maxX:
-			self.x = maxX - 10
-		elif self.x < 0:
-			self.x = 0
+    def moveH(self, s, maxX):
+        self.x += s
+        if self.x > maxX:
+            self.x = maxX - 10
+        elif self.x < 0:
+            self.x = 0
+
 
 class Enemy(Player, object):
-	def __init__(self, x, y):
-		super(Enemy,self).__init__(x,y)
-		self.x = x
-		self.y = y
+    def __init__(self, x, y):
+        super(Enemy, self).__init__(x, y)
+        self.x = x
+        self.y = y
 
-	def getDistTo(self, player):
-		xdiff = self.getXDistTo(player)
-		ydiff = self.getYDistTo(player)
-		return math.sqrt(xdiff*xdiff + ydiff*ydiff)
+    def getDistTo(self, player):
+        xdiff = self.getXDistTo(player)
+        ydiff = self.getYDistTo(player)
+        return math.sqrt(xdiff * xdiff + ydiff * ydiff)
 
-	def getXDistTo(self, player):
-		xdiff = self.x - player.x
-		return xdiff
+    def getXDistTo(self, player):
+        xdiff = self.x - player.x
+        return xdiff
 
-	def getYDistTo(self, player):
-		ydiff = self.y - player.y
-		return ydiff
+    def getYDistTo(self, player):
+        ydiff = self.y - player.y
+        return ydiff
